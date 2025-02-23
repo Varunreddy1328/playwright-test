cb48615435bedaa3c0a53d7fdea6c09269040897
varun <kesireddyvarunreddy08@gmail.com>
2025-02-23 10:43:12 +0530

    change in code

--- tests/facebookLogin.test.js
+++ tests/facebookLogin.test.js
@@ -9,7 +9,10 @@ test.describe('Facebook Login Tests', () => {
         await loginPage.login('wrongemail@example.com', 'wrongpassword');  // Replace with incorrect details
 
         // Wait for the error message to appear
-        const errorMessage = page.locator('._9ay7'); // Updated selector
-        await expect(errorMessage).toBeVisible({ timeout: 10000 });  // Increased timeout to 10s
+        // const errorMessage = page.locator('._9ay5'); // Updated selector
+        // await expect(errorMessage).toBeVisible({ timeout: 10000 });  // Increased timeout to 10s
+        const errorMessage = page.locator('text="The email address you entered isn\'t connected to an account."');
+        await expect(errorMessage).toBeVisible();
+
     });
 });
